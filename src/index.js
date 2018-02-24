c312597a2f8adbf91ea51193a4bf0a3bb98f1d2b
vlukyaneRubyLabs <vlukyane@gmail.com>
2018-02-24 20:11:25 +0300

    fair solution

--- src/index.js
+++ src/index.js
@@ -1,39 +1,52 @@
 class SmartCalculator {
   constructor(initialValue) {
-    this.str = initialValue;
+    this.str = [initialValue];
+    this.prev_pow_was_one = false;
   }
 
   toString() {
-    var math = require('mathjs');
-    return math.eval(this.str);
+    var expr = this.str.join().replace(/,/g , "");
+    return eval(expr)
   }
 
   add(number) {
-    this.str += '+' + number;
+    if (this.prev_pow_was_one) { this.prev_pow_was_one = false; }
+    this.str.push('+');
+    this.str.push(number);
     return this;
   }
   
   subtract(number) {
-    this.str += '-' + number;
+    if (this.prev_pow_was_one) { this.prev_pow_was_one = false; }
+    this.str.push('-');
+    this.str.push(number);
     return this;
   }
 
   multiply(number) {
-    this.str += '*' + number;
+    if (this.prev_pow_was_one) { this.prev_pow_was_one = false; }
+    this.str.push('*');
+    this.str.push(number);
     return this;
   }
 
   devide(number) {
-    this.str += '/' + number;
+    if (this.prev_pow_was_one) { this.prev_pow_was_one = false; }
+    this.str.push('/');
+    this.str.push(number);
     return this;
   }
 
   pow(number) {
-    this.str += '^' + number;
+    if (this.prev_pow_was_one) return this;
+    if (number === 1){ this.prev_pow_was_one = true; return this; }
+    var n = this.str.length;
+    var calc = this.str[n - 1];
+    for (var i = 0; i < number - 1; i++){ calc *= this.str[n - 1]; }
+    this.str[n - 1] = calc;
     return this;
   }
 }
 
-
-
+//console.log(value.toString());
 module.exports = SmartCalculator;
